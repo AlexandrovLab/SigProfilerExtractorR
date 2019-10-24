0346b7805bd9c7519bdfd8588f66232cefded04c
mishu <mishugeb1@hotmail.com>
2019-10-23 20:24:31 -0700

    readme

--- R/sigpro.R
+++ R/sigpro.R
@@ -1,24 +1,32 @@
 
-#' SigprofilerExtractor Function
+#' sigprofilerextractor
+#' @description Extracts De Novo signature from genomes and Decomposes the De Novo Signatures to Cosmic Signatures
 #'
-#' @param input_type The type of input
-#' @param output The name/path of the result folder
-#' @param input Input file or folder
-#' @param refgen Reference genome
-#' @param genome_build Genome opportunity
-#' @param minsigs Minimum number of signatures to be extracted
-#' @param maxsigs Maximum number of signatures to be extracted
-#' @param replicates Number of replication of signature extraction
-#' @param mtype Context type
-#' @param init NMF initialization
-#' @param exome Exome true of False
-#' @param cpu Number of CPUs to be used during multiprocessing
+#' @param input_type input_type: A string. Type of input. The type of input should be one of the following:\cr
+#' ----- "vcf": used for vcf format inputs.\cr
+#' ----- "table": used for table format inputs using a tab seperated file.\cr
+
+#' @param output A string. The name of the output folder. The output folder will be generated in the current working directory.\cr
+#' @param inputdata A string. Name of the input folder (in case of "vcf" type input) or the input file (in case of "table"  type input).\cr
+#' The project file or folder should be inside the current working directory.\cr
+#' For the "vcf" type input,the project has to be a folder which will contain the vcf files in vcf format or text formats.\cr
+#' The "text"type projects have to be a file.
+#' @param refgen A string, optional. The name of the reference genome. The default reference genome is "GRCh37". This parameter is applicable only if the input_type is "vcf".\cr
+#' @param genome_build
+#' @param minsigs A positive integer, optional. The minimum number of signatures to be extracted. The default value is 1
+#' @param maxsigs A positive integer, optional. The maximum number of signatures to be extracted. The default value is 10
+#' @param replicates A positive integer, optional. The number of iteration to be performed to extract each number signature. The default value is 8
+#' @param mtype A  strings, optional. The items in the list defines the mutational contexts to be considered to extract the signatures. The default value is "96,DINUCS,ID",\cr
+#'  where "96" is the SBS96 context, "DINUC" is the DINULEOTIDE context and ID is INDEL context. Each context should be written in a single string separated by "," without any space.
+#' @param init A string. Represents the NMF initialization. Options are "random", "nndsvdar" and "nndsvd"
+#' @param exome Boolean, optional. Defines if the exomes will be extracted. The default value is "False".
+#' @param cpu An integer, optional. The number of processors to be used to extract the signatures. The default value is -1 which will use all available processors.
 #'
 #' @return a folder with results
-#' @export sigProfilerExtractor
+#' @export sigprofilerextractor
 #'
 #' @examples
-sigProfilerExtractor <- function(input_type, output, input, refgen = 'GRCh37', genome_build="GRCh37",minsigs = 1, maxsigs = 3,  replicates=5, mtype = c('96,DINUCS,ID'), init="random",exome=F, cpu=-1) {
+sigprofilerextractor <- function(input_type, output, inputdata, refgen = 'GRCh37', genome_build="GRCh37",minsigs = 1, maxsigs = 3,  replicates=5, mtype = c('96,DINUCS,ID'), init="random",exome=F, cpu=-1) {
 
   sys <- reticulate::import("sys")
   sigpro <- reticulate::import("sigproextractor.sigpro")
@@ -29,7 +37,7 @@ sigProfilerExtractor <- function(input_type, output, input, refgen = 'GRCh37', g
   replicates=as.integer(replicates)
   exome=FALSE
   cpu = as.integer(cpu)
-  sigpro$sigProfilerExtractor(input_type, output, input, refgen = 'GRCh38',genome_build="GRCh38",startProcess=minsigs, endProcess = maxsigs, totalIterations=replicates, exome=exome, init=init)
+  sigpro$sigProfilerExtractor(input_type, output, inputdata, refgen = 'GRCh38',genome_build="GRCh38",startProcess=minsigs, endProcess = maxsigs, totalIterations=replicates, exome=exome, init=init)
   sys$stdout$flush()
 
 }
@@ -46,6 +54,57 @@ sigProfilerExtractor <- function(input_type, output, input, refgen = 'GRCh37', g
 #' @examples
 importdata <- function(datatype){
   sigpro <- reticulate::import("sigproextractor.sigpro")
-  project = sigpro$importdata("vcf")
+  project = sigpro$importdata(datatype)
   return(project)
+}
+
+
+
+#' decomposition
+#' @description Decomposes the De Novo Signatures to Cosmic Signatures
+#' @param signatures A string. Path to a  tab delimited file that contains the signaure table where the rows are mutation types and colunms are signature IDs.
+#' @param activities A string. Path to a tab delimilted file that contains the activity table where the rows are sample IDs and colunms are signature IDs.
+#' @param samples  A string. Path to a tab delimilted file that contains the activity table where the rows are mutation types and colunms are sample IDs.
+#' @param output A string. Path to the output folder.
+#' @param mutation_type A string. The context type. Example: "96", "192", "1536", "6144", "INDEL", "DINUC". The default value is "96".
+#' @param genome_build A string. The genome type. Example: "GRCh37", "GRCh38", "mm9", "mm10". The default value is "GRCh37"
+#' @param verbose Boolean. Prints statements. Default value is False.
+#'
+#' @return A folder with decomposition results The files below will be generated in the output folder: \cr
+#'    \cr
+#'    1.comparison_with_global_ID_signatures.csv \cr
+#     2.Decomposed_Solution_Activities.txt \cr
+#'    3.Decomposed_Solution_Samples_stats.txt \cr
+#'    4.Decomposed_Solution_Signatures.txt \cr
+#'    5.decomposition_logfile.txt \cr
+#'    6.Mutation_Probabilities.txt \cr
+#'    7.Signature_assaignment_logfile.txt \cr
+#'    8.Signature_plot[MutatutionContext]_plots_Decomposed_Solution.pdf
+#' @export decomposition
+#'
+#' @examples
+decomposition <- function(signatures, activities, samples, output, mutation_type="96", genome_build="GRCh37", verbose=F){
+  decompose <-reticulate::import("sigproextractor.decomposition")
+  result = decomposition$decompose(signatures, activities, samples, output, mutation_type="96", genome_build="GRCh37", verbose=False)
+  return(result)
+}
+
+#' Title
+#'
+#' @param genome
+#' @param custom
+#' @param rsync
+#' @param bash
+#' @param ftp
+#'
+#' @return
+#' @export install
+#'
+#' @examples
+install <- function(genome, custom=F, rsync=F, bash=T, ftp=T){
+  os <- reticulate::import("os")
+  sys <- reticulate::import("sys")
+  genInstall <- reticulate::import("SigProfilerMatrixGenerator.install")
+  genInstall$install(genome, custom, rsync, bash, ftp)
+  sys$stdout$flush()
 }
--- README.md
+++ README.md
@@ -1,1 +1,94 @@
+[![Docs](https://img.shields.io/badge/docs-latest-blue.svg)](https://osf.io/s93d5/wiki/home/) [![License](https://img.shields.io/badge/License-BSD\%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause) [![Build Status](https://travis-ci.com/AlexandrovLab/SigProfilerMatrixGeneratorR.svg?branch=master)](https://travis-ci.com/AlexandrovLab/SigProfilerMatrixGeneratorR)
+
 # SigProfilerExtractorR
+An R wrapper for running the SigProfilerExtractor framework.
+
+**INTRODUCTION**
+
+The purpose of this document is to provide a guide for using the SigProfilerExtractor framework to extract the De Novo mutational signatures from a set of samples and decompose the De Novo signatures into the COSMIC signatures. An extensive Wiki page detailing the usage of this tool can be found at https://osf.io/t6j7u/wiki/home/. 
+
+For users that prefer working in a Python environment, the tool is written in Python and can be found and installed from: https://github.com/AlexandrovLab/SigProfilerExtractor
+
+**PREREQUISITES**
+
+devtools  (R) 
+```
+>> install.packages("devtools")
+```
+reticulate* (R) 
+```
+>> install.packages("reticulate")  
+```
+
+*Reticulate has a known bug of preventing python print statements from flushing to standard out. As a result, some of the typical progress messages are delayed.
+
+**QUICK START GUIDE**
+
+This section will guide you through the minimum steps required to extract mutational signatures from genomes:
+1. First, install the python package using pip. The R wrapper still requires the python package:
+```
+                          pip install sigproextractor
+```
+2. Open an R session and ensure that your R interpreter recognizes the path to your python installation:
+```
+$ R
+>> library("reticulate")
+>> use_python("path_to_your_python")
+>> py_config()
+python:         /anaconda3/bin/python
+libpython:      /anaconda3/lib/libpython3.6m.dylib
+pythonhome:     /anaconda3:/anaconda3
+version:        3.6.5 |Anaconda, Inc.| (default, Apr 26 2018, 08:42:37)  [GCC 4.2.1 Compatible Clang 4.0.1 (tags/RELEASE_401/final)]
+numpy:          /anaconda3/lib/python3.6/site-packages/numpy
+numpy_version:  1.16.1
+```
+If you do not see your python path listed, restart your R session and rerun the above commands in order.
+
+2. Install SigProfilerExtractorR using devtools:
+```
+>>library("devtools")
+>>install_github("AlexandrovLab/SigProfilerExtractorR")
+```
+3. Load the package in the same R session and install your desired reference genome as follows (available reference genomes are: GRCh37, GRCh38, mm9, and mm10):
+```
+>> library("SigProfilerExtractorR")
+>> install("GRCh37", rsync=FALSE, bash=TRUE)
+```
+
+This will install the human 37 assembly as a reference genome.
+
+**SUPPORTED GENOMES**
+
+Information about supported will be found at https://github.com/AlexandrovLab/SigProfilerMatrixGeneratorR
+
+**Extraction Signatures**
+
+Signatures can be extracted from vcf files or tab delimited mutational table using the sigprofilerextractor function.
+```
+>> help(sigprofilerextractor)
+```
+This will show the details about the sigprofilerextractor funtion.
+
+Example:
+```
+>> library("SigProfilerExtractorR")
+>> path_to_example_data <- importdata("table")
+>> data <- path_to_example_data # here you can provide the path of your own data
+>> sigprofilerextractor("table", "example_output", data, minsigs=1, maxsigs=3, replicates=10, cpu=-1)
+```
+
+The example file will generated in the working directory
+
+
+**Decomposition of signature**
+SigProfilerExtractorR offer a separate function, decomposition, that decomposes a tab delimited file containing a set of De Novo signatures to the COSMIC signatures and attribute them to a given set of sample.
+```
+help(decomposition)
+```
+This will show the details about the decomposition function.
+## COPYRIGHT
+This software and its documentation are copyright 2018 as a part of the sigProfiler project. The SigProfilerExtractor framework is free software and is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+## CONTACT INFORMATION
+Please address any queries or bug reports to S M Ashiqul Islam (Mishu) at m0islam.ucsd.edu
+
