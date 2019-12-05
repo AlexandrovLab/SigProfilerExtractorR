dd2b1a0821ad5bc69f6e231281e1befdd99aa6e2
mishugeb <mishugeb1@hotmail.com>
2019-12-04 16:55:38 -0800

    Update sigpro.R

--- R/sigpro.R
+++ R/sigpro.R
@@ -37,8 +37,7 @@ sigprofilerextractor <- function(input_type, output, inputdata, refgen = 'GRCh37
   replicates=as.integer(replicates)
   exome=FALSE
   cpu = as.integer(cpu)
-  print(mtype)
-  sigpro$sigProfilerExtractor(input_type, output, inputdata, refgen = 'GRCh37',genome_build="GRCh37",startProcess=minsigs, endProcess = maxsigs, totalIterations=replicates, mtype=mtype, exome=exome, init=init)
+  sigpro$sigProfilerExtractor(input_type, output, inputdata, refgen = 'GRCh37',genome_build="GRCh37",startProcess=minsigs, endProcess = maxsigs, totalIterations=replicates, mtype="96", exome=exome, init=init)
   sys$stdout$flush()
 
 }
